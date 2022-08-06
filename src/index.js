287c2ac6f26348154c5200ff28881c568d9876b2
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-08-06 19:21:22 +0530

    Added Layout to the Front/Home page and Completed all basic design and layouts including header, main, footer and routing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import "antd/dist/antd.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
