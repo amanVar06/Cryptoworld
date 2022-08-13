0b1449d152db6a9deb3f019cc9592662e56ec7f8
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-08-13 19:04:00 +0530

    Search function implemented on cryptocurrencies page and resolved previous issues still using react to fetch api data not redux toolkit, Link working when we refresh the page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -26,24 +26,48 @@ const options = {
 
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
-  options.params.limit = `${simplified}`;
-  const [cryptos, setCryptos] = useState("");
+  options.params.limit = `${count}`;
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    axios
-      .request(options)
-      .then(function (response) {
-        setCryptos(response.data?.data?.coins);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, [setCryptos, cryptos]);
+    {
+      searchTerm === ""
+        ? axios
+            .request(options)
+            .then(function (response) {
+              setCryptos(response.data?.data?.coins);
+            })
+            .catch(function (error) {
+              console.error(error);
+            })
+        : axios
+            .request(options)
+            .then(function (response) {
+              const filteredData = response.data?.data?.coins.filter((coin) =>
+                coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+              );
+
+              setCryptos(filteredData);
+            })
+            .catch(function (error) {
+              console.error(error);
+            });
+    }
+  }, [setCryptos, cryptos, searchTerm]);
 
   // console.log(cryptos);
 
   return (
     <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
